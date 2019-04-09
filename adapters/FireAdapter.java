d535b5da5c9703ef03ca43a213577abefdbf1baf
NanoClem <clemdecoop.pro@gmail.com>
2019-04-10 00:01:51 +0200

    creation de capteurs

---
+++ adapters/FireAdapter.java
@@ -0,0 +1,17 @@
+
+/**
+ * Adapter de l'interface FireListener.
+ * <p>
+ * Implemente FireListener et definit avec un corps vide ses methodes
+ * Un ecouteur n'aura besoin que de redefinir uniqument la methode qu'il utilise, et non toutes
+ * </p>
+ *
+ * @author decoopmc
+ * @version 1.0
+ * @see FireListener
+ */
+public abstract class FireAdapter implements FireListener {
+  public void criticalLevelChanged(int oldLevel, int newLevel)   {}
+  public void criticalLevelRaised(int oldLevel, int newLevel)    {}
+  public void criticalLevelDecreased(int oldLevel, int newLevel) {}
+}
---
+++ captors/Captor.java
@@ -0,0 +1,20 @@
+
+/**
+ * <b>CLASSE MERE DE TOUS LES DIFFERENTS TYPES DE CAPTEURS</b>
+ *
+ * @author decoopmc
+ * @version 1.0
+ */
+ abstract class Captor {
+
+   /**
+    * Identifiant du capteur.
+    * Il est unique pour chaque capteur.
+    */
+   private static int id;
+
+   /*
+    * Lieu ou se trouve le capteur.
+    */
+   private String location;
+ }
