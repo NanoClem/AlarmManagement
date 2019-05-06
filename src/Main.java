69c93f3ece61c85b4244549d59532e21158b3cec
NanoClem <clemdecoop.pro@gmail.com>
2019-05-06 17:09:00 +0200

    Warning issue

--- src/Main.java
+++ src/Main.java
@@ -14,14 +14,17 @@ public class Main
      */
     public static void main(String[] args) throws Exception
     {
+        //WARNING : COUPLAGE FORT ENTRE LES FENÊTRES
+        //une alarme générée depuis une fenêtre doit pouvoir être affichée sur les deux fenêtres
         MainWindow w = new MainWindow("AlarmManagement");
-
+        MainWindow w2 = new MainWindow("AlarmManagement");
         /*================================================
             TEST MONITEURS
         ================================================*/
         Monitor FireStation    = new Monitor("A");
         Monitor EcologyControl = new Monitor("B");
         w.addMonitor(FireStation);
-        w.addMonitor(EcologyControl);
+        w2.addMonitor(FireStation);
+        w2.addMonitor(EcologyControl);
     }
 }
